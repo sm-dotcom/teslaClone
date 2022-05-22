6e31cd7564f96a50345d0f0653134f279a448e16
macbook <sarahmahmood@protonmail.com>
2022-05-22 17:27:03 +0500

    Components created

---
+++ components/CarItem/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View, Text, ImageBackground} from 'react-native';
+import styles from "./style";
+
+const CarItem = (props) => {
+    return (
+        <View style={styles.carContainer}>
+            <ImageBackground
+                source ={require('../../assets/images/ModelS.jpeg')}
+                style={styles.image}
+            />
+
+            <View style={styles.titles}>
+
+                <Text style={styles.title}>Model S</Text>
+
+                <Text style={styles.subtitle}>Starting at $69,420</Text>
+
+            </View>
+
+        </View>
+
+    );
+};
+
+export default CarItem;
---
+++ components/CarItem/style.js
@@ -0,0 +1,39 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+
+    carContainer: {
+        width: '100%',
+        height: '100%',
+
+    },
+
+    titles: {
+        marginTop: '30%',
+        width: '100%',
+        alignItems: 'center'
+
+    },
+
+    title: {
+        fontsize: 100,
+        fontWeight: '400',
+    },
+
+    subtitle: {
+        fontsize: 16,
+        color: '#5c5e62'
+
+    },
+
+    image: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+        position: 'absolute',
+
+    }
+
+});
+
+export default styles;
